6c59105a6f379de052843b6f0b8d633fdd171a83
Nicolas Romano <nicoezequielromano98@gmail.com>
2025-10-01 12:48:32 -0300

    corrigiendo cors

--- Backend/src/utils/cors.js
+++ Backend/src/utils/cors.js
@@ -1,6 +1,6 @@
 const corsOptions = {
-  //  origin: ["https://catalogoayp.vercel.app"],
-  origin: ["http://localhost:5173"],
+  origin: ["https://catalogoayp.vercel.app"],
+  //origin: ["http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
