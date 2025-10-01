94a93942f0ffd178aab11a047d3f10ea54417b53
Nicolas Romano <nicoezequielromano98@gmail.com>
2025-10-01 12:52:25 -0300

    actualizando deploy

--- Backend/src/utils/cors.js
+++ Backend/src/utils/cors.js
@@ -1,6 +1,5 @@
 const corsOptions = {
   origin: ["https://catalogoayp.vercel.app"],
-  //origin: ["http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://catalogoayp-production.up.railway.app/api",
-  //baseURL: "http://localhost:8080/api",
 });
 
 API.interceptors.request.use((req) => {
